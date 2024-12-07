e5e500503d9bab414ad17e930dcb3df2fa034415
Nikita Smirov <nikitka-smirnov-2013@mail.ru>
2024-12-07 18:15:38 +0300

    перенес styles из shared в app

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app/App';
-import './shared/styles/_global.scss';
+import './app/styles/_global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
